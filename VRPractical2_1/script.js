b4e233b7857fc3c1fe57063e21868e269fc23618
valeryo9 <valeryo9@nycstudents.net>
2025-10-17 02:33:45 +0000

    .

--- VRPractical2_1/script.js
+++ VRPractical2_1/script.js
@@ -7,11 +7,6 @@ function rnd(l, u){
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene"); //CSS Selector
 
-  for(let i = 0; i < 15; i++){
-    let x = rnd(-20,20);
-    let z = rnd(-20,20);
-    createTree(x,0,z);
-  }
    //Task 2: Use the createCloud(...)  to add several clouds to the scene at various positions.
   for(let i = 0; i < 30; i++){
     let x = rnd(-20, 20);
@@ -94,24 +89,6 @@ function createHouse(x, z){
 }
 
 
-function createTree(x, y, z){
-  let tree = document.createElement("a-entity");
-  
-  let pines = document.createElement("a-cone");
-  pines.setAttribute("color","green");
-  pines.setAttribute("position","0 2 0");
-  pines.setAttribute("height","2");
-  tree.append( pines );
-
-  let stump = document.createElement("a-cylinder");
-  stump.setAttribute("position","0 1 0");
-  stump.setAttribute("color","brown");
-  stump.setAttribute("radius","0.25");
-  tree.append( stump );
-
-  tree.setAttribute("position",{x:x, y:y, z:z});
-  scene.append( tree )
-}
 
 
 
