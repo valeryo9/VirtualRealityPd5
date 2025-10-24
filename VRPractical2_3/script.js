6c2a2bec1415ac00ba370f182221ec3e8c3c6336
valeryo9 <valeryo9@nycstudents.net>
2025-10-24 03:31:41 +0000

    .

--- VRPractical2_3/script.js
+++ VRPractical2_3/script.js
@@ -4,7 +4,7 @@ let scene;
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene"); //CSS Selector
 
-  for(let i = 0; i < 100; i++){
+  for(let i = 0; i < 70; i++){
     let x = rnd(-20,20);
     let z = rnd(-20,20);
     let tree = new Tree(x, 0 , z);
