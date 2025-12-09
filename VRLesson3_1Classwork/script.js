551361755b1b9a32e03b92591cba4d222ac8fd9d
valeryo9 <valeryo9@nycstudents.net>
2025-12-09 02:40:00 +0000

    .

--- VRLesson3_1Classwork/script.js
+++ VRLesson3_1Classwork/script.js
@@ -1,12 +1,19 @@
-window.addEventListener("DOMContentLoaded",function() {
+window.addEventListener("DOMContentLoaded", function() {
   let myBox = document.querySelector("#myBox");
-  //Task 3: Create a variable for the cylinder.  Get the element from the HTML
+  // Task 3: Create a variable for the cylinder. Get the element from the HTML
+  let myCylinder = document.querySelector("#myCylinder");
 
+  myBox.addEventListener("mouseenter", function() {
+    this.setAttribute("height", 1.5);
+  });
 
-  myBox.addEventListener("mouseenter",function(){
-    this.setAttribute("height",2);
-  })
-  //Task 4: Add another event to set the height of myBox back to 1 when the mouse is no longer on myBox
+  // Task 4: Add another event to set the height of myBox back to 1 when the mouse is no longer on myBox
+  myBox.addEventListener("mouseleave", function() {
+    this.setAttribute("height", 1);
+  });
 
-  //Task 5: Make  the the cylinder disappear when you click on it.  Set opacity to zero
-})
+  // Task 5: Make the cylinder disappear when you click on it. Set opacity to zero
+  myCylinder.addEventListener("click", function() {
+    this.setAttribute("opacity", 0);
+  });
+});
